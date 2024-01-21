3442e0ffe9b2932f8da780a516bf455e31bba6a9
gschamika <97242725+gschamika@users.noreply.github.com>
2024-01-21 17:57:48 +0530

    commit 1

---
+++ src/main/java/com/scg/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.scg.fullstackbackend.repository;
+
+import com.scg.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
