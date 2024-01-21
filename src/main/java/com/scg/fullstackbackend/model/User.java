39be453382f4cd2af739b0cd3f74767492c68208
gschamika <97242725+gschamika@users.noreply.github.com>
2024-01-21 17:42:30 +0530

    commit 1

--- src/main/java/com/scg/fullstackbackend/model/User.java
+++ src/main/java/com/scg/fullstackbackend/model/User.java
@@ -1,2 +1,47 @@
-package com.scg.fullstackbackend.model;public class user {
+package com.scg.fullstackbackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String username;
+    private String name;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
