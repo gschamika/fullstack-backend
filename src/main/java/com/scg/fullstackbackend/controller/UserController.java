cc2631f7ceb72d5ea44285cdde068ff892bf05f9
gschamika <97242725+gschamika@users.noreply.github.com>
2024-01-21 18:13:49 +0530

    commit 1

---
+++ src/main/java/com/scg/fullstackbackend/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.scg.fullstackbackend.controller;
+
+import com.scg.fullstackbackend.model.User;
+import com.scg.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+}
