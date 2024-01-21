d7830dc7c7f189a0c64506d035fe36a484317ba4
gschamika <97242725+gschamika@users.noreply.github.com>
2024-01-21 19:38:44 +0530

    commit 1

--- src/main/java/com/scg/fullstackbackend/controller/UserController.java
+++ src/main/java/com/scg/fullstackbackend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.scg.fullstackbackend.controller;
 import com.scg.fullstackbackend.model.User;
 import com.scg.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
